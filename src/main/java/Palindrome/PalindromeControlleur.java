60e4d5f4379138f08b6ece5028f3779e070fbbb5
charles nseke <nsekecharles@gmail.com>
2016-01-19 21:19:48 +0100

    Palindrome kata

--- src/main/java/Palindrome/PalindromeControlleur.java
+++ src/main/java/Palindrome/PalindromeControlleur.java
@@ -4,4 +4,44 @@ package Palindrome;
  * Created by nsekecharles on 19/01/2016.
  */
 public class PalindromeControlleur {
+
+    public boolean estUnPalindrome(String chaine) {
+
+        if("".equals(chaine) || chaine == null) {
+
+            return true;
+
+        } else {
+
+            String reversed = new StringBuilder(chaine).reverse().toString();
+
+            return reversed.equals(chaine);
+        }
+
+    }
+
+    public boolean estUnPalindrome2(String chaine) {
+
+        if("".equals(chaine) || chaine == null) {
+
+            return true;
+
+        } else {
+
+            int fin = chaine.length() - 1;
+
+            int milieu = chaine.length() / 2;
+
+            for(int i= 0; i < milieu; i++, fin--) {
+
+                if(chaine.charAt(i) != chaine.charAt(fin)) {
+
+                    return false;
+
+                }
+            }
+
+            return true;
+        }
+    }
 }
--- src/test/java/PalindromeControlleurTest.java
+++ src/test/java/PalindromeControlleurTest.java
@@ -1,6 +1,10 @@
 import Palindrome.PalindromeControlleur;
+import org.junit.Assert;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
 
 /**
  * Created by nsekecharles on 19/01/2016.
@@ -25,5 +29,64 @@ public class PalindromeControlleurTest {
     @After
     public void tearDown() {
         palindrome = null;
+    }
+
+    /**
+     * Test qu'une chaine vide retourne true quand on vérifie s'il s'agit d'un palindrome
+     */
+    @Test
+    public void test_UneChaineVide_Retourne_True() {
+
+        Assert.assertTrue(palindrome.estUnPalindrome(""));
+    }
+
+    /**
+     * Test qu'une chaine nulle retourne true
+     */
+
+    @Test
+    public void test_UneChaineNulle_Retourne_True() {
+
+
+        Assert.assertTrue(palindrome.estUnPalindrome(null));
+
+
+    }
+
+    @Test
+    public void test_LaChaine_aba_Retourne_True() {
+
+        long debut = Calendar.getInstance().getTimeInMillis();
+
+        Assert.assertTrue(palindrome.estUnPalindrome2("aba"));
+
+        long fin = Calendar.getInstance().getTimeInMillis();
+
+      //  System.out.println("Executé en : " + (fin - debut) + " milis") ;
+    }
+
+    @Test
+    public void test_LaChaine_abba_Retourne_True() {
+
+        long debut = Calendar.getInstance().getTimeInMillis();
+
+        Assert.assertTrue(palindrome.estUnPalindrome2("abba"));
+
+        long fin = Calendar.getInstance().getTimeInMillis();
+
+        System.out.println("Executé en : " + (fin - debut) + " milis") ;
+
+    }
+
+    @Test
+    public void test_LaChaine_test_Retourne_False() {
+
+        long debut = Calendar.getInstance().getTimeInMillis();
+
+        Assert.assertFalse(palindrome.estUnPalindrome2("test"));
+
+        long fin = Calendar.getInstance().getTimeInMillis();
+
+        System.out.println("Executé en : " + (fin - debut) + " milis") ;
     }
 }
